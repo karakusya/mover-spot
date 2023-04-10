6d579e79f91946de8f406016834bfe46f658a0ca
karakusya <katty67kuc@gmail.com>
2023-04-11 00:38:18 +0300

    swiper demo

---
+++ swiper-script.js
@@ -0,0 +1,14 @@
+new Swiper('.image-slider',{
+    navigation: {
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev'
+    },
+    slidesPerView: 1,
+            spaceBetween: 150,
+            slidesPerGroup: 1,
+            centerSlidesBounds: true,
+            centerInsufficientSlides: true,
+            centerSlides: true,
+            loop: true,
+            loopFillGroupWithBlank: true,
+});
